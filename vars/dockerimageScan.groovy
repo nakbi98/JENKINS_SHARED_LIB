b6dc00c0fe91cf4272c40dffd987b4a4b7066724
nakbi98 <elbuut422@gmail.com>
2023-11-06 08:57:25 +0100

    add exception dockerscan

--- vars/dockerimageScan.groovy
+++ vars/dockerimageScan.groovy
@@ -1,7 +1,11 @@
 def call(String project, String ImageTag, String hubUser) {
-    sh """
-
-       trivy image ${hubUser.toLowerCase()}/${project}:${ImageTag} ${hubUser.toLowerCase()}/${project}:latest > scan.txt
-       cat scan.txt
-    """
+    try {
+        sh """
+           trivy image ${hubUser.toLowerCase()}/${project}:${ImageTag} ${hubUser.toLowerCase()}/${project}:latest > scan.txt
+           cat scan.txt
+        """
+    } catch (Exception e) {
+        currentBuild.result = 'FAILURE'
+        error("Échec de l'analyse de sécurité de l'image : ${e.getMessage()}")
+    }
 }
