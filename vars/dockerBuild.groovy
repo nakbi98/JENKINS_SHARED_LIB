110116cb234375218e5dae7e318238d5d0c8f37b
nakbi98 <elbuut422@gmail.com>
2023-11-06 07:15:30 +0100

    dockerfunc

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,16 +1,15 @@
-def call(String project, String ImageTag, String hubUser){
+/*def call(String project, String ImageTag, String hubUser){
     sh  """
        docker image build -t ${hubUser}/${project},
        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag},
        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
 
     """
-}
-/*/
+}*/
+
 def call(String project, String ImageTag, String hubUser) {
     sh """
        docker image build -t ${hubUser}/${project}:${ImageTag} .
        docker image tag ${hubUser}/${project}:${ImageTag} ${hubUser}/${project}:latest
     """
 }
-*/
